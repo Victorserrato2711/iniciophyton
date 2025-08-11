3c074af0e5b996314d5b127d1173f5862a1d4fb9
Victorserrato2711 <victor.g2714@gmail.com>
2025-08-11 17:28:36 -0600

    Add files via upload
    Mis primeros pasos para generar un sistema de reservacion en phyton

---
+++ reservacion_hotel.py
@@ -0,0 +1,30 @@
+print(f'*** Reservacion Hotel ***')
+
+#Variables Fijas
+costos_vista_mar = 190.50 #por Dia
+costo_sin_vista = 150.50 #por dia
+
+#Datos solicitados al Cliente
+nombre_cliente = str(input(f'Cual es su nombre?: '))
+dias_estadia = int(input(f'Cuantos dias seria su hospedaje?: '))
+cuarto_con_vista = input('Desea Cuarto con vista al mar (Si/No)?: ')
+cuarto_con_vista = cuarto_con_vista.strip().lower()
+
+#Costos
+total_vista = dias_estadia * costos_vista_mar
+total_sin_vista = dias_estadia * costo_sin_vista
+
+#Validacion de datos
+if not cuarto_con_vista == 'si':
+    print(f'''\n ------------- Detalles de la Reservacion -------------
+    Hola {nombre_cliente}
+    Selecciono una cuarto sin vista al mar por un total de 
+    {dias_estadia} dias 
+    El Monto total Seria de ${total_sin_vista:.2f}''')
+
+else:
+    print(f''' \n ------------- Detalles de la Reservacion -------------
+    Hola {nombre_cliente}
+    Selecciono una cuarto Con vista al mar por un total de 
+    {dias_estadia} dias 
+    El Monto total Seria de ${total_vista:.2f}''')
